50b54d47b6e803032a8791b7aa9024c8302ea1d0
王正东 <wangzhengdong@ecmoban.com>
2019-10-24 10:17:31 +0800

    优化Facades

--- Royalcms/Component/Pinyin/Facades/Pinyin.php
+++ Royalcms/Component/Pinyin/Facades/Pinyin.php
@@ -3,26 +3,25 @@
 namespace Royalcms\Component\Pinyin\Facades;
 
 use Royalcms\Component\Support\Facades\Facade;
-use Royalcms\Component\Pinyin\Pinyin;
+use Royalcms\Component\Pinyin\Pinyin as MyPinyin;
 
-class PinyinFacade extends Facade
+class Pinyin extends Facade
 {
 
-    const POLICY_CAMEL = Pinyin::POLICY_CAMEL;
+    const POLICY_CAMEL = MyPinyin::POLICY_CAMEL;
 
-    const POLICY_STUDLY = Pinyin::POLICY_STUDLY;
+    const POLICY_STUDLY = MyPinyin::POLICY_STUDLY;
 
-    const POLICY_UNDERSCORE = Pinyin::POLICY_UNDERSCORE;
+    const POLICY_UNDERSCORE = MyPinyin::POLICY_UNDERSCORE;
 
-    const POLICY_HYPHEN = Pinyin::POLICY_HYPHEN;
+    const POLICY_HYPHEN = MyPinyin::POLICY_HYPHEN;
 
-    const POLICY_BLANK = Pinyin::POLICY_BLANK;
+    const POLICY_BLANK = MyPinyin::POLICY_BLANK;
 
-    const POLICY_SHRINK = Pinyin::POLICY_SHRINK;
+    const POLICY_SHRINK = MyPinyin::POLICY_SHRINK;
 
     /**
      * Get the registered name of the component.
-     *
      * @return string
      */
     protected static function getFacadeAccessor()
